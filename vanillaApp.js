b2789375321fbc604b4f7bf234ea8b0111732739
grantkee <grant.d.kee@gmail.com>
2019-06-06 22:46:40 -0500

    recoding todo list in vanilla javaScript

---
+++ vanillaApp.js
@@ -0,0 +1,10 @@
+//creating objects to target HTML
+const formID = document.getElementById('formID');
+const listID = document.getElementById('listID');
+
+//creating function to add tiles when submit button is pushed
+
+function submitButton(e){
+    e.preventDefault();
+    const itemText = ()
+}
