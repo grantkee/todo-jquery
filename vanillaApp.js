62023754a9569a4de7ea19bea1005b6e9a12a60b
grantkee <grant.d.kee@gmail.com>
2019-06-09 12:08:18 -0500

    saving vanilla edits, still not close

--- vanillaApp.js
+++ vanillaApp.js
@@ -7,4 +7,17 @@ const listID = document.getElementById('listID');
 function submitButton(e){
     e.preventDefault();
     const itemText = ()
+    
 }
+
+//I need to create a function that posts the input text from the form and write's it in the </ul>
+
+function placeMarker()
+
+//I'm declaring the variable "postTodo" which will equal what the user puts in the form's text.input (html id = postTodo)
+
+const postTodo = document.getElementById('postTodo');
+
+//creating write function to post 
+
+document.getElementById("listID").innerHTML = postTodo;
